de2efb669eaac9b4e1777383a2377ef37b2742d6
Trevor Jameson <github email address>
2019-01-17 14:41:24 -0500

    multiple solutions passing

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,43 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Tests passing
+// function palindrome(str) {
+//     for (let i = 0; i < str.length / 2; i++) {
+//         if (str[i] !== str[str.length - 1 - i]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// Tests passing
+// function palindrome(str) {
+//     for (let char of str) {
+//         let i = str.indexOf(char)
+//         if (char !== str[str.length - 1 - i]) {
+//             return false
+//         }
+//     }
+//     return true
+// }
+
+// Tests passing
+// function palindrome(str) {
+//     const firstHalfStr = str.slice(0, Math.floor((str.length - 1) / 2)).split('').reverse().join('')
+//     const secondHalfStr =  str.slice(Math.ceil((str.length) / 2))
+//     return firstHalfStr === secondHalfStr
+// }
+
+// Tests passing
+// function palindrome(str) {
+//     const revStr = str.split('').reverse().join('')
+//     return str === revStr
+// }
+
+// Tests passing
+// function palindrome(str) {
+//     return str.split('').every((char, i) => char === str[str.length - i - 1])
+// }
 
 module.exports = palindrome;
